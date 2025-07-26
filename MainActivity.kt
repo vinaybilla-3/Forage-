31376c501d3026c2588b7c5f0e1c9051b45e3f77
vinaybilla-3 <142726415+vinaybilla-3@users.noreply.github.com>
2025-07-26 17:51:08 +0530

    Task 3

---
+++ MainActivity.kt
@@ -0,0 +1,15 @@
+
+package com.skyscanner.flightscry
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import net.skyscanner.backpack.text.BpkText
+import net.skyscanner.backpack.card.BpkCard
+import net.skyscanner.backpack.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
